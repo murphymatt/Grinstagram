c4dcb4b2d605959d44ed190b213dcb929fc61c49
murphymatt <murphyma@grinnell.edu>
2016-02-14 15:09:29 -0600

    Added progress bar and image loader

---
+++ app/src/main/java/com/mattmurphy/grinstagram/ImagesLoader.java
@@ -0,0 +1,9 @@
+package com.mattmurphy.grinstagram;
+
+/**
+ * Created by mattmurphy on 2/14/16.
+ */
+public class ImagesLoader {
+
+
+}
--- app/src/main/java/com/mattmurphy/grinstagram/MainActivity.java
+++ app/src/main/java/com/mattmurphy/grinstagram/MainActivity.java
@@ -1,10 +1,15 @@
 package com.mattmurphy.grinstagram;
 
+import android.app.ActionBar;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,17 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button toNewActivityButton = (Button)findViewById(R.id.new_activity_button);
-        toNewActivityButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                changeActivity(v);
-            }
-        });
+        // create progress bar
+        ProgressBar progressBar = new ProgressBar(this);
+        progressBar.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        progressBar.setIndeterminate(true);
+        ((ListView) findViewById(android.R.id.list)).setEmptyView(progressBar);
+
+        // add progress bar to root of the layout
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        root.addView(progressBar);
+
+
+
     }
 
     public void changeActivity(View view) {
         Intent intent = new Intent(this, ButtonActivity.class);
         startActivity(intent);
     }
+
+
+
 }

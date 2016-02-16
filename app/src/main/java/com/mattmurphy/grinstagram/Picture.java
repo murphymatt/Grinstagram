2ec1db4f351d8c8ddd7b768214aa5aeb8e759e5b
murphymatt <murphyma@grinnell.edu>
2016-02-16 15:03:28 -0600

    added picassa to gradle build created Picture object

---
+++ app/src/main/java/com/mattmurphy/grinstagram/Picture.java
@@ -0,0 +1,48 @@
+package com.mattmurphy.grinstagram;
+
+import android.media.Image;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mattmurphy on 2/16/16.
+ */
+
+@ParseClassName("Picture")
+public class Picture extends ParseObject {
+
+    private Image mImage;
+    private List<String> mComments;
+    private int mLikes;
+
+    public Picture() {
+        mComments = new ArrayList<String>();
+        mLikes = 0;
+    }
+
+    public Image getImage() {
+        return mImage;
+    }
+
+    public void setImage(Image image) {
+        mImage = image;
+    }
+
+    public List<String> getComments() {
+        return mComments;
+    }
+
+    public void addComments(String comment) {
+        mComments.add(comment);
+    }
+
+    public int getLikes() {
+        return mLikes;
+    }
+
+    public void setLikes(int likes) {
+        mLikes = likes;
+    }
+
+}

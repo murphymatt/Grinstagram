8cfd2b28eaff58a791a7742fbb5ea90f34e0e3fc
murphymatt <murphyma@grinnell.edu>
2016-02-18 19:47:11 -0600

    updated gradle build for Parse

--- app/src/main/java/com/mattmurphy/grinstagram/MainActivity.java
+++ app/src/main/java/com/mattmurphy/grinstagram/MainActivity.java
@@ -1,43 +1,44 @@
 package com.mattmurphy.grinstagram;
 
-import android.app.ActionBar;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
 
         // create progress bar
-        ProgressBar progressBar = new ProgressBar(this);
-        progressBar.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+        mProgressBar = new ProgressBar(this);
+        mProgressBar.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT));
-        progressBar.setIndeterminate(true);
-        ((ListView) findViewById(android.R.id.list)).setEmptyView(progressBar);
+        mProgressBar.setIndeterminate(true);
+        ((ListView) findViewById(android.R.id.list)).setEmptyView(mProgressBar);
 
         // add progress bar to root of the layout
         ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
-        root.addView(progressBar);
+        root.addView(mProgressBar);
 
-
+        
 
     }
 
+/*
     public void changeActivity(View view) {
         Intent intent = new Intent(this, ButtonActivity.class);
         startActivity(intent);
     }
-
+*/
 
 
 }

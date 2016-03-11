70395ba2abf360789e6932c852ac5a1786f860f7
murphymatt <murphyma@grinnell.edu>
2016-03-10 20:12:41 -0600

    started likes implementation for button, expandable textview in imageListAdapter

--- app/src/main/java/com/mattmurphy/grinstagram/ImageListAdapter.java
+++ app/src/main/java/com/mattmurphy/grinstagram/ImageListAdapter.java
@@ -9,9 +9,8 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 import com.squareup.picasso.Picasso;
-
-import java.util.List;
 
 /**
  * Created by Mattori on 2/22/16.
@@ -20,10 +19,11 @@ import java.util.List;
  */
 public class ImageListAdapter extends ArrayAdapter<Picture> {
 
+
     public ImageListAdapter(Context context, int resource) {
         super(context, resource);
-
     }
+
     /**
      * TODO: set the contents of image, like, and share based on Firebase
      * @param pos Position in array
@@ -31,21 +31,36 @@ public class ImageListAdapter extends ArrayAdapter<Picture> {
      * @param parent The parent ViewGroup
      * @return The image list item's View
      */
+
     public View getView(int pos, View convertView, ViewGroup parent) {
         if(convertView == null) {
             convertView = LayoutInflater.from(this.getContext()).inflate(R.layout.image_list_item, parent, false);
         }
 
-        Picture pic = ImageListAdapter.super.getItem(pos);
+        final Picture pic = ImageListAdapter.super.getItem(pos);
 //        TextView tv = (TextView) convertView.findViewById(R.id.tv);
 //        tv.setText(pic.getImageUrl());
 
         if (pic != null) {
             ImageView image = (ImageView) convertView.findViewById(R.id.image);
             ImageButton like = (ImageButton) convertView.findViewById(R.id.like);
-            /* Create comments button */
+            TextView likeNum = (TextView) convertView.findViewById(R.id.likeNum);
+            likeNum.setText(pic.getLikes());
+
+            // when like button is clicked, increment the likes
+            like.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    pic.incrementLikes();
+                }
+            });
+
+            ExpandableRelativeLayout commentsView
+                    = (ExpandableRelativeLayout) convertView.findViewById(R.id.expandableLayout);
+            TextView comments = (TextView) convertView.findViewById(R.id.text);
 
             Picasso.with(getContext()).load(pic.getImageUrl()).into(image);
+            //comments.setText(pic.getComments());
         }
 
         return convertView;

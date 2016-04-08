4b8c48d3f2f455de8c58cd451f0cda2bc2c32170
murphymatt <murphyma@grinnell.edu>
2016-04-07 22:57:21 -0500

    make add comment button show up in CommentActivity.java

--- app/src/main/java/com/mattmurphy/grinstagram/CommentActivity.java
+++ app/src/main/java/com/mattmurphy/grinstagram/CommentActivity.java
@@ -1,27 +1,35 @@
 package com.mattmurphy.grinstagram;
 
-import android.media.Image;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
-
-import java.util.List;
 
 public class CommentActivity extends AppCompatActivity {
 
+    Button addComment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
-        ListView list = (ListView) findViewById(android.R.id.list);
-        CommentListAdapter adapter = new CommentListAdapter(this, R.layout.comment_list_view);
-        adapter.addAll(getIntent().getStringArrayListExtra(ImageListAdapter.EXTRA_COMMENTS));
-        list.setAdapter(adapter);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // create list and adapter from xml
+        ListView list = (ListView) findViewById(android.R.id.list);
+        final CommentListAdapter adapter = new CommentListAdapter(this, R.layout.comment_list_view);
+        adapter.addAll(getIntent().getStringArrayListExtra(ImageListAdapter.EXTRA_COMMENTS));
+        if (adapter != null) {
+            list.setAdapter(adapter);
+        }
+
+        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        addComment = (Button) findViewById(R.id.add_comment);
+        addComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.add("New comment");
+            }
+        });
     }
 }
--- app/src/main/java/com/mattmurphy/grinstagram/CommentListAdapter.java
+++ app/src/main/java/com/mattmurphy/grinstagram/CommentListAdapter.java
@@ -12,18 +12,21 @@ import android.widget.TextView;
  * Created by Mattori on 4/7/16.
  */
 public class CommentListAdapter extends ArrayAdapter<String> {
-    public CommentListAdapter(Context context, int resource) { super(context, resource); }
+
+    public CommentListAdapter(Context context, int resource) {
+        super(context, resource);
+    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if(convertView == null) {
+        if (convertView == null) {
             convertView = LayoutInflater.from(this.getContext()).inflate(R.layout.comment_list_view, parent, false);
         }
 
         String comment = CommentListAdapter.super.getItem(position);
         Log.d("comment", comment);
 
-        if(comment != null) {
+        if (comment != null) {
             Log.d("comment", "comment is not null");
             TextView content = (TextView) convertView.findViewById(android.R.id.content);
             content.setText(comment);

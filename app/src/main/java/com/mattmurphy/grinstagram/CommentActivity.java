ef171a68c0a982924342b2398d18e22382503a72
murphymatt <murphyma@grinnell.edu>
2016-04-13 23:55:39 -0500

    custom comments can now be added via user input

--- app/src/main/java/com/mattmurphy/grinstagram/CommentActivity.java
+++ app/src/main/java/com/mattmurphy/grinstagram/CommentActivity.java
@@ -2,13 +2,14 @@ package com.mattmurphy.grinstagram;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class CommentActivity extends AppCompatActivity {
 
-    Button addComment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,17 +19,17 @@ public class CommentActivity extends AppCompatActivity {
         ListView list = (ListView) findViewById(android.R.id.list);
         final CommentListAdapter adapter = new CommentListAdapter(this, R.layout.comment_list_view);
         adapter.addAll(getIntent().getStringArrayListExtra(ImageListAdapter.EXTRA_COMMENTS));
-        if (adapter != null) {
-            list.setAdapter(adapter);
-        }
+        if (adapter != null) { list.setAdapter(adapter); }
 
-        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        addComment = (Button) findViewById(R.id.add_comment);
+        Button addComment = (Button) findViewById(R.id.add_comment);
+        final EditText commenting = (EditText) findViewById(R.id.edit_comment);
         addComment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                adapter.add("New comment");
+                adapter.add(commenting.getText().toString());
+                commenting.getText().clear();
             }
         });
     }
